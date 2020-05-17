342f8005521ae88c64ed5e133ebed9c9797e0972
Carreira Fernando <fe.alves_carreira@hotmail.com>
2020-05-16 22:00:00 -0300

    transforma-binario em decimal e vise-versa

--- conversor-binario/conversor.py
+++ conversor-binario/conversor.py
@@ -1,11 +1,33 @@
-while True:
+def binario_decimal():
     decimal = 0
     binario = str(input('Digite um número em binario "sair, para terminar": ')).strip().lower()
-    if binario == 'sair':
-        break
     cont = len(binario)
     for i in binario:
         if i == '1':
             decimal += 2 ** (cont - 1)
         cont -= 1
-    print(decimal)
+    return decimal
+
+def decimal_binario():
+    binario = []
+    decimal = int(input('Digite um número Decimal: '))
+    numero = decimal
+    while True:
+        if numero == 0 or numero == 1:
+            break
+        binario.append(numero % 2)
+        numero = numero // 2
+    if numero == 1:
+        binario.append(numero)
+    binario.reverse()
+    return binario
+
+while True:
+    print('Digite "sair" para terminar!')
+    escolha = str(input('Quer tranformar par Binário ou Decimal [B] ou [D]: ')).lower()
+    if escolha == 'sair':
+        break
+    if escolha == 'b':
+        print(decimal_binario())
+    elif escolha == 'd':
+        print(binario_decimal())
